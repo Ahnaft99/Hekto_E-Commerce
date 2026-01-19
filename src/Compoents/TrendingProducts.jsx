1b35dbf685b8f7fa2b3c258ef26a91acca04aee1
Ahnaf Tahmid <ahnaf.ent@gmail.com>
2026-01-20 01:09:16 +0600

    trending product card

--- src/Compoents/TrendingProducts.jsx
+++ src/Compoents/TrendingProducts.jsx
@@ -1,8 +1,9 @@
 import Container from "../Compoents/Container"
 import React, { useEffect } from 'react'
-import chair from '../assets/chair.png'
+import bag from '../assets/bag.png'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "./Slice/ProductSlice";
+import scale from '../assets/scale.png'
 
 
 function TrendingProducts() {
@@ -47,6 +48,56 @@ function TrendingProducts() {
                             </div>
                         ))}
                     </div>
+                            <div className="w-full flex gap-4 py-25 items-center">
+
+                           
+                    <div className="w-4/12  ">
+                        <div className="w-82.5 h-80 bg-[#FFF6FB] shadow-2xl">
+                            <h2 className="font-josefin text-[20px] font-semibold py-2 ml-4">23% off in all products</h2>
+                           <a className="text-[#FB2E86] font-semibold ml-4 font-josefin" href="#">Shop Now</a>
+                           <div className="w-full ml-37">
+                           <img className="text-right" src={bag} alt="" />
+                           </div>
+                          
+                        </div>
+                    
+                        </div> 
+                            <div className="w-4/12">
+                            <div className="w-82.5 h-80 bg-[#FFF6FB] shadow-2xl">
+                            <h2 className="font-josefin text-[20px] font-semibold py-2 ml-4">23% off in all products</h2>
+                           <a className="text-[#FB2E86] font-semibold ml-4 font-josefin" href="#">Shop Now</a>
+                           <div className="w-full ">
+                           <img className="w-62.5 ml-20 mt-10" src={scale} alt="" />
+                           </div>
+                          
+                        </div>
+                            </div>
+                            <div className="w-4/12 ">
+                            {data?.slice(0,3).map(product=>(
+                                <>
+                                   <div className="w-82.5 h-26 bg-white shadow-2xl mb-2 flex items-center ">
+                                    <img className="h-18" src={product.thumbnail} alt="" />
+                                    <div className="">
+                                        <p>{product.title}</p>
+                                        <p>{product.price}</p>
+                                    </div>
+                                </div>
+                                {/* <div className="w-82.5 h-20 bg-white shadow-2xl mb-2">
+                                    <img className="h-18" src={product.thumbnail} alt="" />
+                                </div>
+                                <div className="w-82.5 h-20 bg-white shadow-2xl ">
+                                    <img className="h-18" src={product.thumbnail} alt="" />
+                                </div> */}
+                                </>
+                             
+                                
+                            ))}
+                                
+                            </div>
+                        </div>
+                     
+                        
+                    
 
 
                 </Container>
